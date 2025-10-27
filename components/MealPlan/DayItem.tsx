b643bbd4c28b2cceb876b4f169807e96e2c98f7e
Alexander McLean-Janet <alexander.mcleanjanet@diffuse.ly>
2025-10-27 21:26:20 +0100

    fix ui meal planner

--- components/MealPlan/DayItem.tsx
+++ components/MealPlan/DayItem.tsx
@@ -21,82 +21,106 @@ const DayItemTags = ({ tags }: { tags?: any[] }) => {
   if (!tags || tags.length === 0) return null
 
   return (
-    <YStack gap="$2" alignItems="flex-end">
+    <XStack gap="$2" flexWrap="wrap" mt="$1">
       {tags.map((tag) => (
         <XStack
           key={tag.id}
           backgroundColor="white"
-          paddingHorizontal="$2"
-          paddingVertical="$0.5"
+          paddingHorizontal="$1.5"
           borderRadius="$1"
           alignItems="center"
-          gap="$2"
+          gap="$1"
         >
-          <Paragraph fontSize="$3" color="black">
+          <Paragraph fontSize="$2" color="black">
             {tag.name}
           </Paragraph>
         </XStack>
       ))}
-    </YStack>
+    </XStack>
   )
 }
 
 export const DayItem = ({ item, onSwap }: DayItemProps) => {
   return (
-    <Card
-      elevate
-      size="$4"
-      bordered
-      mb="$2"
-      mx="$2"
-    >
-      <Card.Header padded>
-        <XStack alignItems="center" justifyContent="space-between" gap="$3">
-          <XStack alignItems="center" gap="$3" f={1}>
+    <YStack mb="$3" mx="$2">
+      {/* Day Header with Name and Tags */}
+      <XStack
+        p="$3"
+        backgroundColor="$gray9"
+        borderTopLeftRadius="$4"
+        borderTopRightRadius="$4"
+        borderWidth={1}
+        borderBottomWidth={0}
+        borderColor="$borderColor"
+      >
+        <Paragraph fontWeight="700" size="$5" color="$color" mr="auto">
+          {item.dayOfWeek}
+        </Paragraph>
+        <DayItemTags tags={item.tags} />
+      </XStack>
+
+      {/* Recipe Card */}
+      <Card
+        elevate
+        bordered
+        borderTopLeftRadius={0}
+        borderTopRightRadius={0}
+        borderTopWidth={0}
+        minHeight={90}
+      >
+        <Card.Header padded>
+          <XStack alignItems="center" gap="$3">
+            {/* Recipe Image */}
             {item.recipe?.imageUrl ? (
               <Image
                 source={{ uri: item.recipe.imageUrl }}
-                width={50}
-                height={50}
-                borderRadius="$2"
+                width={60}
+                height={60}
+                borderRadius="$3"
               />
             ) : (
               <YStack
-                width={50}
-                height={50}
-                backgroundColor="$gray5"
-                borderRadius="$2"
+                width={60}
+                height={60}
+                backgroundColor="$gray4"
+                borderRadius="$3"
                 alignItems="center"
                 justifyContent="center"
               >
                 <Text fontSize="$2" color="$gray10" textAlign="center">
-                  No Recipe
+                  No{'\n'}Recipe
                 </Text>
               </YStack>
             )}
+
+            {/* Recipe Info */}
             <YStack f={1}>
-              <Paragraph fontWeight="600" size="$4">
-                {item.dayOfWeek}
-              </Paragraph>
-              <Paragraph color="$color" size="$3">
+              <Paragraph
+                fontWeight="600"
+                size="$4"
+                color="$color"
+                numberOfLines={2}
+              >
                 {item.recipe?.name || 'No recipe assigned'}
               </Paragraph>
-                <DayItemTags tags={item.tags} />
-
+              {item.recipe && (
+                <Paragraph size="$2" color="$gray11" mt="$1">
+                  Tap refresh to change
+                </Paragraph>
+              )}
             </YStack>
+
+            {/* Swap Button */}
+            <Button
+              size="$3"
+              variant="outlined"
+              icon={RefreshCw}
+              onPress={() => onSwap(item)}
+              circular
+            />
           </XStack>
-
-
-          <Button
-            size="$3"
-            variant="outlined"
-            icon={RefreshCw}
-            onPress={() => onSwap(item)}
-            circular
-          />
-        </XStack>
-      </Card.Header>
-    </Card>
+        </Card.Header>
+      </Card>
+    </YStack>
   )
 }
-
